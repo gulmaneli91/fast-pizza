16cb6a6c0b04ddfb9d6645693a8d33c1f6b19137
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2023-12-06 14:49:27 -0300

    stylling dysplay

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,13 +10,15 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid-rows-[auto_1fr_auto] grid h-screen ">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
+      <div className="overflow-scroll">
+      <main className=" max-w-3xl  mx-auto">
         <Outlet />
       </main>
+      </div>
       <CartOverview />
     </div>
   );
